c0ef311e86ef694dd3f8f43f3fb8f204cad9fc85
Richard Barran <richard@arbee-design.co.uk>
2014-03-21 16:51:02 +0000

    Manager method is more flexible about what is viewable - needs tests.

--- mininews/managers.py
+++ mininews/managers.py
@@ -3,14 +3,15 @@ from django.db.models import Q
 
 import datetime
 
+
 class ArticleQuerySet(QuerySet):
 
-    def viewable(self):
+    def viewable(self, statuses=['published']):
         """Has an article been published - this is a mix of:
         - status being published.
         - article is within start and end dates.
         """
-        return self.filter(status=self.model.STATUS.published).\
+        return self.filter(status__in=statuses).\
             filter(Q(start__lte=datetime.date.today) | Q(start__isnull=True)).\
             filter(Q(end__gte=datetime.date.today) | Q(end__isnull=True))
 
@@ -18,6 +19,3 @@ class ArticleQuerySet(QuerySet):
         """A convenience method - when listing articles, we'll often want to
         show all the years for which we have articles."""
         return self.model.objects.viewable().dates('start', kind, order='DESC')
-
-
-
