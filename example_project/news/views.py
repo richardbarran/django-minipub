d2f7371804ec9bd3f074b308f461103560b50a8d
David Talbot <david.talbot@pancentric.com>
2014-04-04 20:04:25 +0100

    Remove unused import and make minor pep8 adjustments

--- example_project/news/views.py
+++ example_project/news/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 from mininews.views import ArticleArchiveView, ArticleYearArchiveView, \
-ArticleDetailView
+    ArticleDetailView
 
 from .models import Article
+
 
 class ArticleArchiveView(ArticleArchiveView):
     model = Article
     context_object_name = 'article_list'
 
+
 class ArticleYearArchiveView(ArticleYearArchiveView):
     model = Article
+
 
 class ArticleDetailView(ArticleDetailView):
     model = Article
@@ -20,4 +22,3 @@ class ArticleDetailView(ArticleDetailView):
     def get_object(self):
         article = get_object_or_404(self.model, slug=self.kwargs['slug'])
         return self._allowed(article)
-
