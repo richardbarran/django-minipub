26cf4bae7a4444f9055901667b286a3aeaec0e2f
Richard Barran <richard@arbee-design.co.uk>
2014-09-17 08:41:09 +0100

    Code tidy.

--- example_project/news/urls.py
+++ example_project/news/urls.py
@@ -2,8 +2,13 @@ from django.conf.urls import patterns, url
 from .views import ArticleDetailView, ArticleArchiveView, ArticleYearArchiveView
 
 urlpatterns = patterns('',
-    url(r'^$', ArticleArchiveView.as_view(), name='article_archive'),
-    url(r'^year/(?P<year>\d{4})/$', ArticleYearArchiveView.as_view(), name="article_year"),
-    url(r'^(?P<slug>[\w-]+)/$', ArticleDetailView.as_view(), name='article_detail'),
-
-)
+                       url(r'^$',
+                           ArticleArchiveView.as_view(),
+                           name='article_archive'),
+                       url(r'^year/(?P<year>\d{4})/$',
+                           ArticleYearArchiveView.as_view(),
+                           name="article_year"),
+                       url(r'^(?P<slug>[\w-]+)/$',
+                           ArticleDetailView.as_view(),
+                           name='article_detail'),
+                       )
