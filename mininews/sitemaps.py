eba712b9efced9cb8d2d6cd0683fb550e5f5b1ca
Richard Barran <richard@arbee-design.co.uk>
2014-09-22 21:02:14 +0100

    Remove defaults from the sitemap.py that should not be set in Mininews.

--- mininews/sitemaps.py
+++ mininews/sitemaps.py
@@ -3,12 +3,6 @@ from django.contrib.sitemaps import Sitemap
 
 class MininewsSitemap(Sitemap):
 
-    # Let's assume that an article - once published - will not change.
-    changefreq = "never"
-
-    # Define the model class here to make it easier to customise this class.
-    model = None
-
     def items(self):
         return self.model.objects.live()
 
