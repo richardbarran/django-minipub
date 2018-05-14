6e18af51805f1a2c97f4512fa8ebf8c218db8206
Gabriel - Iulian Dumbrava <gabriel.dumbrava@gmail.com>
2018-05-14 12:24:29 +0300

    Fix for pip 10.

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #/usr/bin/env python
 
 import uuid
-from setuptools import setup, find_packages
-from pip.req import parse_requirements
-
 import minipub
+
+from setuptools import setup, find_packages
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 
 def get_requirements(source):
